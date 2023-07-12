a1d55f2a741b6a032f284727918f9ca38a0095dc
deboraa07 <debcamilly00@gmail.com>
2023-07-11 22:30:44 -0300

    ajusta cadastro de usuario

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,26 +1,53 @@
 const Usuario = require('../models/Usuario')
+const neo4j = require('neo4j-driver');
 
-const cadastrarUsuario = async (req, res) => {
-    try {
-      const usuario = await Usuario.create(req.body);
-      console.log(usuario);
-       // Exibe o usuário cadastrado no console
-      res.status(200).send(usuario);
-    } catch (error) {
-      console.error(error);
-      res.status(400).send(error);
-    }
-  };
+const url = 'neo4j://localhost';
+const driver = neo4j.driver(url, neo4j.auth.basic('neo4j', 'neo4j123*'));
 
-  const fazerLogin =  async (req, res) => {
-    const { username, password } = req.body;
-  
+
+const criarUsuario = async (req, res) => {
   try {
-    const usuario = await Usuario.findOne({ username: username, password: password });
-    
+    const { nome, email, senha } = req.body;
+
+    const session = driver.session();
+
+    // Verificar se o usuário já existe no Neo4j
+    const existeUsuario = await session.run(
+      'MATCH (u:Usuario {email: $email}) RETURN COUNT(u) AS count',
+      { email }
+    );
+
+    const count = existeUsuario.records[0].get('count').toNumber();
+    if (count > 0) {
+      return res.status(400).send('O usuário já existe no Neo4j.');
+    }
+
+    // Criar o usuário no Neo4j
+    const result = await session.run(
+      'CREATE (u:Usuario {nome: $nome, email: $email, senha:$senha})',
+      { nome, email, senha }
+    );
+    console.log(nome);
+
+    session.close();
+    driver.close();
+
+    res.status(200).send('Usuário criado');
+  } catch (error) {
+    console.error('Erro ao criar usuário no Neo4j:', error);
+    res.status(500).send('Erro ao criar usuário no Neo4j.');
+  }
+};
+
+const fazerLogin = async (req, res) => {
+  const { nome, senha } = req.body;
+
+  try {
+    const usuario = await Usuario.findOne({ nome: nome, senha: senha});
+
     if (usuario) {
       console.log(usuario); // Exibir informações do usuário no console
-      res.send('Login bem-sucedido! Redirecionar para a página principal...');
+      res.send('Login bem-sucedido!');
     } else {
       res.send('Credenciais inválidas. Tente novamente...');
     }
@@ -31,4 +58,6 @@ const cadastrarUsuario = async (req, res) => {
 };
 
 
-  module.exports = {cadastrarUsuario, fazerLogin}
+module.exports = {
+  criarUsuario, fazerLogin
+}
--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const usuarioController = require('../controllers/UsuarioController');
+
 //USUARIO//
-router.post('/', usuarioController.cadastrarUsuario);
-router.post('/', usuarioController.fazerLogin);
+router.post('/', usuarioController.criarUsuario);
+router.post('/', usuarioController.fazerLogin); 
 
 module.exports = router;
